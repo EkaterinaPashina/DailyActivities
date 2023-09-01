a356d3b572e4e57a33b12cdbe2527a85da03a93a
EkaterinaPashina <k377at@mail.ru>
2023-09-01 13:29:05 +0500

    from the outset

---
+++ my-app/src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Title
+} from 'chart.js';
+
+import {
+  Pie
+} from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend, Title);
+
+const data = {
+  labels: [
+    'Work',
+    'Eat',
+    'Commute',
+    'Watch TV',
+    'Sleep',
+  ],
+  datasets: [{
+    label: 'Percent per day',
+    data: [45.8, 8.3, 8.3, 8.3, 29.2],
+    backgroundColor: [
+      'rgb(68, 104, 201)',
+      'rgb(206, 63, 29)',
+      'rgb(242, 154, 31)',
+      'rgb(61, 148, 33)',
+      'rgb(143, 27, 150)',
+    ],
+    hoverOffset: 4
+
+  }],
+}
+
+const options = {
+  title: {
+    display: true,
+    text: 'MY DAILY ACTIVITIES',
+    position: 'top',
+    fontSize: 16,
+    fontColor: '#555',
+    padding: 20
+  },
+};
+
+
+function App() {
+  return ( <
+    Pie data = {
+      data
+    }
+    options = {
+      options
+    }
+    />);
+  }
+
+  export default App;
